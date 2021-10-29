7f8e3122f163e70fe3037b9e8ba54ed80d3586da
Thomas <quentin.sebring@icloud.com>
2021-10-29 04:10:55 -0500

    Courses pages added and scaffolded

---
+++ ThomasUniversity/ThomasUniversity/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ThomasUniversity.Data;
+using ThomasUniversity.Models;
+
+namespace ThomasUniversity.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ThomasUniversity.Data.SchoolContext _context;
+
+        public CreateModel(ThomasUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ThomasUniversity/ThomasUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ThomasUniversity.Data;
+using ThomasUniversity.Models;
+
+namespace ThomasUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ThomasUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ThomasUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Courses { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
