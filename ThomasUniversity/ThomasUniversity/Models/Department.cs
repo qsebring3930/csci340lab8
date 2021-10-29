3ef87794df7dbbc4aa646d00e4d22d384d550783
Thomas <quentin.sebring@icloud.com>
2021-10-29 05:08:16 -0500

    Department pages scaffolded

--- ThomasUniversity/ThomasUniversity/Models/Department.cs
+++ ThomasUniversity/ThomasUniversity/Models/Department.cs
@@ -24,6 +24,8 @@ namespace ThomasUniversity.Models
 
         public int? InstructorID { get; set; }
 
+        public Guid ConcurrencyToken { get; set; } = Guid.NewGuid();
+
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
---
+++ ThomasUniversity/ThomasUniversity/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ThomasUniversity.Data;
+using ThomasUniversity.Models;
+
+namespace ThomasUniversity.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ThomasUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ThomasUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+        }
+    }
+}
