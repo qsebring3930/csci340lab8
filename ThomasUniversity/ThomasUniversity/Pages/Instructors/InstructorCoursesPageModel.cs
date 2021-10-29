c131f4d23262de2f4d167725b0ab88d3230685f8
Thomas <quentin.sebring@icloud.com>
2021-10-29 05:02:46 -0500

    Instructor pages updated

---
+++ ThomasUniversity/ThomasUniversity/Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,32 @@
+﻿using ThomasUniversity.Data;
+using ThomasUniversity.Models;
+using ThomasUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThomasUniversity.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseData> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context,
+                                               Instructor instructor)
+        {
+            var allCourses = context.Courses;
+            var instructorCourses = new HashSet<int>(
+                instructor.Courses.Select(c => c.CourseID));
+            AssignedCourseDataList = new List<AssignedCourseData>();
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+    }
+}
